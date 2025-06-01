a73b90c575d9ef742cc571a7b679bbf898e2d07e
edwineduuh770@gmail.com <edwineduuh770@gmail.com>
2025-06-01 21:53:23 +0300

    Define data models

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+# Create a router and register our viewsets with it
 router = DefaultRouter()
-router.register(r'conversations', views.ConversationViewSet, basename='conversation')
-router.register(r'messages', views.MessageViewSet, basename='message')
+router.register(r'conversations', views.ConversationViewSet)
+router.register(r'messages', views.MessageViewSet)
 
+# The API URLs are now determined automatically by the router
 urlpatterns = [
     path('', include(router.urls)),
 ]
